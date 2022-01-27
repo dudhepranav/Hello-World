639f1109911f71725b0595bb3ba506bc8733906c
Pranav Dudhe <79367923+dudhepranav@users.noreply.github.com>
2022-01-27 23:18:06 +0530

    Initial commit of Math.py

---
+++ Math.py
@@ -0,0 +1,18 @@
+#Add implementation
+def add(x,y):
+    return x+y
+
+#Substract implementation   
+def substract(x,y):
+    return x-y #Imply mul MAster branch
+
+#Multiply implementation    
+def multiply(x,y):
+    return x*y #Imply mul over A123 Branch
+
+#Divide implementation    
+def divide(x,y):
+    if(y==0)
+        return INVALID 
+    else 
+        return x/y #On MAster Branch
